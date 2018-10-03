3a38c376729247a5981542f41bbe1f8a400efe72
Daniel Holroyd <cybertether@gmail.com>
2018-10-03 15:00:07 -0400

    Made dictionary more readable

--- natoalpha.py
+++ natoalpha.py
@@ -1,6 +1,42 @@
 #! /usr/bin/python3
 
-dict = {'a':'Alpha', 'b':'Bravo', 'c':'Charlie', 'd':'Delta', 'e':'Echo', 'f':'Foxtrot', 'g':'Golf', 'h':'Hotel', 'i':'India', 'j':'Juliet', 'k':'Kilo', 'l':'Lima', 'm':'Mike', 'n':'November', 'o':'Oscar', 'p':'Papa', 'q':'Quebec', 'r':'Romeo', 's':'Sierra', 't':'Tango', 'u':'Uniform', 'v':'Victor', 'w':'Whiskey', 'x':'X-ray', 'y':'Yankee', 'z':'Zulu', ' ':' ', '1':'One', '2':'Two', '3':'Three', '4':'Four', '5':'Five', '6':'Six', '7':'Seven', '8':'Eight', '9':'Niner', '0':'Zero'}
+dict = {'a':'Alpha',
+ 'b':'Bravo',
+ 'c':'Charlie',
+ 'd':'Delta',
+ 'e':'Echo',
+ 'f':'Foxtrot',
+ 'g':'Golf',
+ 'h':'Hotel',
+ 'i':'India',
+ 'j':'Juliet',
+ 'k':'Kilo',
+ 'l':'Lima',
+ 'm':'Mike',
+ 'n':'November',
+ 'o':'Oscar',
+ 'p':'Papa',
+ 'q':'Quebec',
+ 'r':'Romeo',
+ 's':'Sierra',
+ 't':'Tango',
+ 'u':'Uniform',
+ 'v':'Victor',
+ 'w':'Whiskey',
+ 'x':'X-ray',
+ 'y':'Yankee',
+ 'z':'Zulu',
+ ' ':' ',
+ '1':'One',
+ '2':'Two',
+ '3':'Three',
+ '4':'Four',
+ '5':'Five',
+ '6':'Six',
+ '7':'Seven',
+ '8':'Eight',
+ '9':'Niner',
+ '0':'Zero'}
 
 def nato(string):
   for item in string.lower():
